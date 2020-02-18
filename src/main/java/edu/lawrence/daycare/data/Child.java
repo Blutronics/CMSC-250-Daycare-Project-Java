50ab047c1e31629b4e6ecb7497dd6c25f99d0ff2
Blutronics <toanbum2000@gmail.com>
2020-02-18 17:25:34 -0600

    Added in class for Child and Registration

---
+++ src/main/java/edu/lawrence/daycare/data/Child.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.lawrence.daycare.data;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Khanh Toan
+ */
+public class Child {
+
+    private int id;
+    private int parentID;
+    private String name;
+    private Date birthday = new Date();
+
+    public Child() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getParentID() {
+        return parentID;
+    }
+
+    public void setParentID(int parentID) {
+        this.parentID = parentID;
+    }
+
+    public Date getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Date birthday) {
+        this.birthday = birthday;
+    }
+    
+}
---
+++ src/main/java/edu/lawrence/daycare/data/registration.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.lawrence.daycare.data;
+
+import java.util.Date;
+
+/**
+ *
+ * @author Khanh Toan
+ */
+public class registration {
+
+    private int id;
+    private int parentID;
+    private int childID;
+    private int providerID;
+    private Date start;
+    private Date end;
+    private int status;
+
+    public registration() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getChildID() {
+        return childID;
+    }
+
+    public void setChildID(int childID) {
+        this.childID = childID;
+    }
+
+    public int getParentID() {
+        return parentID;
+    }
+
+    public void setParentID(int parentID) {
+        this.parentID = parentID;
+    }
+
+    public int getProviderID() {
+        return providerID;
+    }
+
+    public void setProviderID(int providerID) {
+        this.providerID = providerID;
+    }
+
+    public int getStatus() {
+        return status;
+    }
+
+    public void setStatus(int status) {
+        this.status = status;
+    }
+
+    public Date getStart() {
+        return start;
+    }
+
+    public void setStart(Date Start) {
+        this.start = Start;
+    }
+
+    public Date getEnd() {
+        return end;
+    }
+
+    public void setEnd(Date end) {
+        this.end = end;
+    }
+}

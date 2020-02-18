ea091093b1dbe11b2e4d79ddbe8213a497602fd1
Blutronics <toanbum2000@gmail.com>
2020-02-18 17:41:00 -0600

    Added in RowMapper for Child and registration

--- src/main/java/edu/lawrence/daycare/data/ChildRowMapper.java
+++ src/main/java/edu/lawrence/daycare/data/ChildRowMapper.java
@@ -4,11 +4,21 @@
  * and open the template in the editor.
  */
 package edu.lawrence.daycare.data;
-
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import org.springframework.jdbc.core.RowMapper;
 /**
  *
  * @author Khanh Toan
  */
-public class ChildRowMapper {
-    
+public class ChildRowMapper implements RowMapper<Child> {
+    @Override
+    public Child mapRow(ResultSet row, int rowNum) throws SQLException {
+        Child c = new Child();
+        c.setId(row.getInt("id"));
+        c.setName(row.getString("name"));
+        c.setParentID(row.getInt("parent"));
+        c.setBirthday(row.getDate("birthday"));
+        return c;
+    }
 }
--- src/main/java/edu/lawrence/daycare/data/registrationRowMapper.java
+++ src/main/java/edu/lawrence/daycare/data/registrationRowMapper.java
@@ -4,11 +4,23 @@
  * and open the template in the editor.
  */
 package edu.lawrence.daycare.data;
-
+import org.springframework.jdbc.core.RowMapper;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 /**
  *
  * @author Khanh Toan
  */
-public class registrationRowMapper {
-    
+public class registrationRowMapper implements RowMapper<registration> {
+    @Override
+    public registration mapRow(ResultSet row, int rowNum) throws SQLException {
+        registration r = new registration();
+        r.setId(row.getInt("id"));
+        r.setChildID(row.getInt("child"));
+        r.setProviderID(row.getInt("provider"));
+        r.setStart(row.getDate("start"));
+        r.setEnd(row.getDate("end"));
+        r.setStatus(row.getInt("status"));
+        return r;
+    }
 }
